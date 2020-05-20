c84f3c290f60acfd101f9580c8466027a82beb93
Christoph Abert <christoph.abert@telekom.de>
2020-05-20 17:15:03 +0200

    Rest Methods introduced, ToDo Debugging Database, Swagger UI

--- src/main/java/rest/ItemRestController.java
+++ src/main/java/rest/ItemRestController.java
@@ -1,17 +1,25 @@
 package rest;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import entities.Item;
 import repo.ItemRepository;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1")
 public class ItemRestController {
 
 	private final ItemRepository repo;
@@ -26,10 +34,32 @@ public class ItemRestController {
 	public String hi() {
 		return "Hi There, I'm here!";
 	}
-	
+
 	@GetMapping("items")
 	public List<Item> getAll() {
 		return repo.findAll();
+	}
+
+	@GetMapping("items/{id}")
+	public Optional<Item> getById(@PathVariable("id") Long id) {
+		return repo.findById(id);
+	}
+
+	@PostMapping("items")
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public Item saveItem(@RequestBody Item item) {
+		return repo.save(item);
+	}
+
+	@PutMapping("items/{id}")
+	public Item putItem(@RequestBody Item item) {
+		return this.repo.save(item);
+	}
+
+	@DeleteMapping("items/{id}")
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void deleteItem(@PathVariable Long id) {
+		repo.deleteById(id);
 	}
 
 }
